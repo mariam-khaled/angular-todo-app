c2dd746449091d1d0f8cf3666a1196fcd87e1b35
mariam-khaled <36280726+mariam-khaled@users.noreply.github.com>
2021-01-27 01:59:22 +0200

    fix quote marks line 23

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'todo-app'`, () => {
+  it('should have as title todo-app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('todo-app');
