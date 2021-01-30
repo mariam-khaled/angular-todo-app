28f42dda93d34774afb8468cc89e76a16966900d
mariam-khaled <36280726+mariam-khaled@users.noreply.github.com>
2021-01-30 20:06:41 +0200

    add missing ; to 1st line
    good checkin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { HomeComponent } from "./home/home.component"
+import { HomeComponent } from "./home/home.component";
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { UpdatetaskComponent } from "./updatetask/updatetask.component";
